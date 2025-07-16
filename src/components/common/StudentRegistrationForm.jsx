e8cabfa67ce6af5206733b6692b11106d5a9771d
Abhiram0304 <tanneriabhi@gmail.com>
2025-07-16 13:01:19 +0530

    Made institute fee receipt comp

--- src/components/common/StudentRegistrationForm.jsx
+++ src/components/common/StudentRegistrationForm.jsx
@@ -123,8 +123,11 @@ const StudentRegistrationForm = () => {
         }else if(!paymentDate){
             toast("Select Hostel Fee Payment Date",{icon:"⚠️"});
             return;
+        }else if(!instituteFeeReceiptResponse){
+            toast("Upload institute Fee Receipt",{icon:"⚠️"});
+            return;
         }else if(!hostelfeeReceiptResponse){
-            toast("Upload Fee Receipt",{icon:"⚠️"});
+            toast("Upload hostel Fee Receipt",{icon:"⚠️"});
             return;
         }else if(!imageResponse){
             toast("Upload your Profile Image",{icon:"⚠️"});
@@ -497,7 +500,7 @@ const StudentRegistrationForm = () => {
                 </div>
                 {/* Institute Fee Receipt */}
                 <div className="md:w-[48%] w-full flex flex-col gap-[0.25rem]">
-                    <label className="font-medium text-black">Institute Fee Receipt :</label>
+                    <label className="font-medium text-black">Institute Fee Receipt <span className="text-xs text-red-600">*</span> :</label>
                     <div className="flex w-full items-center justify-between gap-[1rem]">
                         <input type="file" accept="application/pdf" onChange={pickUpInstituteFeeReceipt} className="max-w-[250px] px-[1rem] py-2 bg-blue-500 text-white font-semibold rounded-md cursor-pointer transition-transform duration-200 hover:scale-105" />
                         {instituteFeeReceiptResponse ? (
