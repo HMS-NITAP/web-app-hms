a6305f72af2450e9c22c319f51944f426aea056b
Abhiram0304 <tanneriabhi@gmail.com>
2025-08-16 23:50:43 +0530

    Update 1

--- src/components/common/AwaitingConfirmation.jsx
+++ src/components/common/AwaitingConfirmation.jsx
@@ -19,8 +19,11 @@ const AwaitingConfirmation = () => {
     <div className="w-full flex flex-col justify-center items-center gap-4">
       <h2 className="text-2xl font-extrabold text-black">Application Submitted!</h2>
       <div className="md:w-[50%] w-full bg-green-100 rounded-2xl p-4 mb-6">
-        <p className="text-center text-base text-black">
+        {/* <p className="text-center text-base text-black">
           Your hostel registration application has been successfully submitted to the hostel office and is currently under review. You will receive a confirmation email at your institute email address, <span className="font-bold">{registrationData?.email}</span>, once all details have been validated. Please note that this process may take 3 – 4 working days. You will be able to log in only after your details have been verified. Thank you for your patience.
+        </p> */}
+        <p className="text-center text-base text-black">
+          Your hostel registration application has been successfully submitted. Kindly proceed to the hostel office Counter 1, with your institute allotment order, fee receipts and a passport size photo.
         </p>
       </div>
       <MainButton text="Done" onPress={handleDone} />
